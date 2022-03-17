d0fb84b0c2d440e40b180c8412f4f2779edb3a0f
alstjr5949 <alstjr5949@gmail.com>
2022-03-18 00:02:10 +0900

    result first

---
+++ src/result.ts
@@ -0,0 +1,78 @@
+export const result = {
+  // 환경 (G)ood or (B)ad
+  // 경험 (N)ewbie or (P)ro
+  // 반려동물 (C)are or (D)on't care
+  // 목적 Plan(T)erior or (H)ealth or (E)motion
+  BNCT: {
+    name: "몬스테라",
+  },
+  BNCH: {
+    name: "스투키",
+  },
+  BNCE: {
+    name: "아글라오네마",
+  },
+  BNDT: {
+    name: "여인초",
+  },
+  BNDH: {
+    name: "필로덴드론",
+  },
+  BNDE: {
+    name: "마리모",
+  },
+  BPCT: {
+    name: "드라세나 맛상게아나",
+  },
+  BPCH: {
+    name: "스킨답서스",
+  },
+  BPCE: {
+    name: "안스리움 안드레아눔",
+  },
+  BPDT: {
+    name: "크로톤",
+  },
+  BPDH: {
+    name: "아가베 아테누아타",
+  },
+  BPDE: {
+    name: "축전",
+  },
+  GNCT: {
+    name: "제라늄",
+  },
+  GNCH: {
+    name: "유칼립투스 구니",
+  },
+  GNCE: {
+    name: "라벤더",
+  },
+  GNDT: {
+    name: "사계귤(유주나무)",
+  },
+  GNDH: {
+    name: "해피트리",
+  },
+  GNDE: {
+    name: "레몬밤",
+  },
+  GPCT: {
+    name: "알로카시아 아마조니카",
+  },
+  GPCH: {
+    name: "뱅갈 고무나무",
+  },
+  GPCE: {
+    name: "율마(월마)",
+  },
+  GPDT: {
+    name: "박쥐란",
+  },
+  GPDH: {
+    name: "파키라",
+  },
+  GPDE: {
+    name: "로즈마리",
+  },
+};
