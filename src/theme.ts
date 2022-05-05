3a719740ab5ff2313efc9731cbaa6eb18928bcad
alstjr5949 <alstjr5949@gmail.com>
2022-05-05 20:21:57 +0900

    반응형 max-width에서 max-device-width로 edit

--- src/theme.ts
+++ src/theme.ts
@@ -6,7 +6,7 @@ const size = {
   IpadPro: "1366px",
   Ipad: "1024px",
   Iphone: "767px",
-  IphoneX: "760px",
+  IphoneX: "700px",
   mobile: "375px",
 };
 
@@ -16,8 +16,8 @@ export const theme: DefaultTheme = {
   IpadPro: `(max-width: ${size.IpadPro})`,
   Ipad: `(max-width: ${size.Ipad})`,
   Iphone: `(max-width: ${size.Iphone})`,
-  IphoneX: `(max-width: ${size.IphoneX})`,
-  mobile: `(max-width: ${size.mobile})`,
+  IphoneX: `(max-device-width: ${size.IphoneX})`,
+  mobile: `(max-device-width: ${size.mobile})`,
   color: {
     green: "#1ac914",
     brown: "#966254",
